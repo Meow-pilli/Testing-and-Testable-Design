790cad8d37827d815f4be90d57adf3b1baa86507
Meow-pilli <149984898+Meow-pilli@users.noreply.github.com>
2024-04-06 05:57:15 -0500

    completed tb

--- HW5/fig11_58_wo_set_tb.v
+++ HW5/fig11_58_wo_set_tb.v
@@ -1,1 +1,21 @@
 `include "fig11_58_wo_set.v"
+module tb_circuit();
+    reg x,CP;
+    wire z;
+
+    circuit uut(x,CP,z);
+
+    initial CP=0;
+    always #5 CP=~CP;
+
+    initial begin
+        $dumpfile("circuit_wo_set.vcd");
+        $dumpvars(0, tb_circuit);
+
+        x<=0;
+        #10
+        x<=1;
+        #10
+        $finish;
+    end
+endmodule
