3f9a8865124d1773c93f59041ec4f73f6f8edecd
Meow-pilli <149984898+Meow-pilli@users.noreply.github.com>
2024-04-15 03:15:42 -0500

    updated directory tmax.tcl

--- HW5/tmax.tcl
+++ HW5/tmax.tcl
@@ -1,7 +1,7 @@
-read_netlist /home/010/s/sx/sxc210186/synopsys/WORK/hw5/class.v -library
-read_netlist /home/010/s/sx/sxc210186/synopsys/WORK/hw5/file_scan.v
+read_netlist ./class.v -library
+read_netlist ./file_scan.v
 run_build_model circuit
-set_drc /home/010/s/sx/sxc210186/synopsys/WORK/hw5/file_scan.stil
+set_drc ./file_scan.stil
 run_drc
 add_faults -all
 run_atpg -ndetects 1
