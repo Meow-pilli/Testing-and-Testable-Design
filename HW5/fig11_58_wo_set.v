04ef22cd7c6d073b326fb34aed40007652b603e8
Meow-pilli <149984898+Meow-pilli@users.noreply.github.com>
2024-04-15 01:26:27 -0500

    corrected ff ref

--- HW5/fig11_58_wo_set.v
+++ HW5/fig11_58_wo_set.v
@@ -1,6 +1,6 @@
 `include "gtech_lib.v"
-module circuit(x,CP,z);
-input x,CP;
+module circuit(x,clk,z);
+input x,clk;
 output z;
 
 wire y1,y2,y3,Y3,c3,c4,c5,c8,c9;
@@ -13,8 +13,8 @@ GTECH_AND2 G5(c5,y3,c9);
 GTECH_AND2 G6(c8,c9,Y3);
 GTECH_NOT G7(c8,z);
 
-GTECH_UDP_FD1 FF1 (y1,c5,CP);
-GTECH_UDP_FD1 FF2 (y2,c8,CP);
-GTECH_UDP_FD1 FF3 (y3,Y3,CP);
+GTECH_UDP_FD1 FF1 (c5,clk, y1);
+GTECH_UDP_FD1 FF2 (c8,clk, y2);
+GTECH_UDP_FD1 FF3 (Y3,clk, y3);
 
 endmodule
