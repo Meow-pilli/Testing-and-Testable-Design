eb210590d53d8b2044351dc6b2e62166be8f1e7b
Meow-pilli <149984898+Meow-pilli@users.noreply.github.com>
2024-04-20 05:36:30 -0500

    first half with bugs q3.py

--- HW6/q3.py
+++ HW6/q3.py
@@ -10,14 +10,15 @@ def add_row(matrix):
     return matrix
 
 def add_column(matrix, func):
-    x_values = np.arange(1, matrix.shape[0] + 1)  # Generate x values from 1 to n
+    x_values = np.arange(0, matrix.shape[0])  # Generate x values from 1 to n
     column_values = func(x_values)  # Calculate column values using the given function
     column_values = column_values.reshape((matrix.shape[0], 1))  # Reshape to column vector
     matrix = np.hstack((matrix, column_values))  # Add the column to the matrix
     return matrix
 
 def function(x):
-    return x**2 + 1   # Function x^2 + 1 for odd values of x, 0 for even values
+    y=((x**2) + 1)
+    return np.where(y % 2 == 0, 0, 1)  # Function x^2 + 1 for odd values of y, 0 for even values
 
 n = 5  # Size of the square matrix
 diagonal_matrix = create_diagonal_matrix(n)
